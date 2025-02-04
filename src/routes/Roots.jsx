cde95b1771251924215a6e1b4c8c9e9677c9c6f7
AjiJeevan <hajeenajeevan@gmail.com>
2025-02-04 13:33:54 +0400

    all files added

--- src/routes/Roots.jsx
+++ src/routes/Roots.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Footer from '../components/shared/Footer'
-import Header from '../components/shared/header'
 import "../styles/homepage.css"
 import { Outlet } from 'react-router-dom'
+import Header from '../components/shared/Header';
 
 function Roots() {
   return (
